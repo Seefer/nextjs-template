c42656987f6e7e064374a99fb30ca2d141fc69a6
Darren Evans <seefer@gmail.com>
2025-01-14 13:37:01 +0000

    ci: edit Biome scripts. Add commitlint package & config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function HomePage() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <h1 className="text-4xl">Hello Next.js World!</h1>
